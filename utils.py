5d3431ed2834f38fd5100400197dbc4f1a7b06a8
Espoir Murhabazi <espoir.mur@gmail.com>
2020-03-17 20:22:22 +0200

    - Task Completed : finsihed uploading zip file
    - Next Task : refractor the code

---
+++ utils.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+def find_by_css(driver, element):
+    """
+    Find element by css
+    
+    Args:
+        driver ([type]): [description]
+        element ([type]): [description]
+    """
+    return driver.find_element(By.CLASS_NAME, element)
+
+
+def find_element_by_name(driver, element):
+    """
+    FInd element by name
+    
+    Args:
+        driver ([type]): [description]
+        element ([type]): [description]
+    """
+    return driver.find_element(By.NAME, element)
+
+
